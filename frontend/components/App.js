f7849463f47fd18faa2558d90aaebbc36f083e6e
Fernando <fmarco@outlook.com>
2024-02-28 10:43:57 +0100

    createCoponent

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import data from './data.json'
+import Header, { foo, bar } from './Header'
+import Main from './Main'
 
 export default function App() {
   console.log(data)
+  console.log(foo, bar);
   return (
     <div>
-      <h1>The Atlantic</h1>
+      <Header />
+      <Main />
     </div>
   )
 }
---
+++ frontend/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+export const foo = 7
+export const bar = 8
+
+export default function Header() {
+    return(
+        <div>Esto es el Header</div>
+    )
+}
